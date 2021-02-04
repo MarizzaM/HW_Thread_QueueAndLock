2bce507899041b726ee36b3f540736cd0144d4ec
User <User@DESKTOP-0BJFLEG>
2021-02-04 12:54:17 +0200

    Add project files.

---
+++ ThreadQueueAndLock/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadQueueAndLock
+{
+    class Program
+    {
+        private static void PrintFrom1To10()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void PrintHW()
+        {
+                Console.WriteLine("Hello world");
+                Thread.Sleep(2000);
+        }
+        static void Main(string[] args)
+        {
+            ThreadExecutor te = new ThreadExecutor();
+
+            te.Add(new Thread(PrintFrom1To10));
+            te.Execute();
+            te.Add(new Thread(PrintHW));
+            te.Execute();
+        }
+
+
+    }
+}
---
+++ ThreadQueueAndLock/ThreadExecutor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadQueueAndLock
+{
+    class ThreadExecutor
+    {
+        Queue<Thread> threadsQueue = new Queue<Thread>();
+
+        public void Add(Thread t) {
+            lock (this)
+            {
+                    threadsQueue.Enqueue(t);
+            }
+        }
+
+        public void Execute() {
+            lock (this) {
+
+                if (threadsQueue.Count != 0) {
+                        Thread t = threadsQueue.Dequeue();
+                        t.Start();
+                }
+            
+            }
+        }
+    }
+}
